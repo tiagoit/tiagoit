cd5233ea7042e9072d8842b975683cd9d00c3f5c
Tiago Ferreira (tiagoit) <tiferreira12@gmail.com>
2019-08-29 14:32:20 -0300

    Initial commit

---
+++ src/js/resume.js
@@ -0,0 +1,34 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  var userLang = navigator.language || navigator.userLanguage;
+  if(userLang.split('-')[0] === 'pt') {
+    $('.lang-pt').removeClass('d-none');  
+  } else {
+    $('.lang-en').removeClass('d-none');  
+  }
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#sideNav'
+  });
+})(jQuery); // End of use strict
