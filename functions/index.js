e89b736d1736eb1f1e14773eb7549d4b0c26930c
Tiago Ferreira (tiagoit) <tiferreira12@gmail.com>
2020-02-03 19:00:33 -0300

    chore: init functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.contact = functions.https.onRequest((request, response) => {
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+  });
+  admin.firestore().collection('contact').add({test: 123});
+ response.send("Hello from Firebase!");
+});
